8f03845957d98279e02181119eff9767471e007d
Your Name <you@example.com>
2021-03-28 22:07:38 +0700

    First

---
+++ app/src/main/java/com/ardhikarafi/mynotesappfix/helper/MappingHelper.kt
@@ -0,0 +1,22 @@
+package com.ardhikarafi.mynotesappfix.helper
+
+import android.database.Cursor
+import com.ardhikarafi.mynotesappfix.db.DatabaseContract
+import com.ardhikarafi.mynotesappfix.entity.Note
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
